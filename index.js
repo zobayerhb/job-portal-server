808c7f29deddbee8843ebed263293c363a34b6c2
zobayerhb <hosenzobayer55@gmail.com>
2024-12-30 11:45:43 +0600

    delete specific jobs api

--- index.js
+++ index.js
@@ -48,12 +48,32 @@ async function run() {
       const email = req.query.email;
       const query = { applicant_email: email };
       const result = await jobsApplication.find(query).toArray();
+
+      // fokira way to aggregate data
+      for (const application of result) {
+        console.log(application.job_id);
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
+          application.title = job.title;
+          application.location = job.location;
+          application.company = job.company;
+          application.company_logo = job.company_logo;
+        }
+      }
       res.send(result);
     });
 
     app.post("/job-applications", async (req, res) => {
       const application = req.body;
       const result = await jobsApplication.insertOne(application);
+      res.send(result);
+    });
+
+    app.delete("/job-application", async (req, res) => {
+      const id = req.query.email;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsApplication.deleteOne(query);
       res.send(result);
     });
 
