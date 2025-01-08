39984a3b949ce6bb108c35eb0fe88e4b36be2c49
zobayerhb <hosenzobayer55@gmail.com>
2025-01-08 22:06:14 +0600

    send client jwt token and get back token from client

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.vpupb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,8 +39,16 @@ async function run() {
     // auth related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign({ user }, "secret", { expiresIn: "1h" });
-      res.send(token);
+      const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "strict",
+        })
+        .send({ status: true });
     });
 
     // jobs related Apis
@@ -75,7 +90,7 @@ async function run() {
       const email = req.query.email;
       const query = { applicant_email: email };
       const result = await jobsApplication.find(query).toArray();
-
+      console.log("cuk cuk tutu", req.cookies);
       // fokira way to aggregate data
       for (const application of result) {
         console.log(application.job_id);
