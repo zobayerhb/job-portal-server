9b4afe0634dc31c4e0367daba0203eb84a42e80d
zobayerhb <hosenzobayer55@gmail.com>
2025-01-09 20:15:15 +0600

    verify token after providing data

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+const verifyToken = (req, res, next) => {
+  // console.log("Verify Token Id: ", req.cookies.token);
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  // verify token
+  jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    next();
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.vpupb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -86,14 +102,13 @@ async function run() {
     // job applications apis
     // get all data , get one, get more (0,1, many)
 
-    app.get("/job-application", async (req, res) => {
+    app.get("/job-application", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
       const result = await jobsApplication.find(query).toArray();
-      console.log("cuk cuk tutu", req.cookies);
       // fokira way to aggregate data
       for (const application of result) {
-        console.log(application.job_id);
+        // console.log(application.job_id);
         const query1 = { _id: new ObjectId(application.job_id) };
         const job = await jobsCollection.findOne(query1);
         if (job) {
